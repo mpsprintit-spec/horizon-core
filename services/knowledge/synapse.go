5782c5968d94a4e9f9f0c35cfeec2ac4044a0b1e
mpsprintit-spec <mpsprintit@gmail.com>
2026-07-18 12:50:02 +0700

    Delete services/knowledge directory

--- services/knowledge/synapse.go
+++
@@ -1,7 +0,0 @@
-package knowledge
-
-// Synapse bertindak sebagai kabel penghubung antar kata yang memiliki beban energi dinamis
-type Synapse struct {
-	TargetNode *ConceptNode
-	Weight     float64
-}
